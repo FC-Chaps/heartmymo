989b7bc2d6fb36e41935dd05a09be439f8650525
benjaminlees <benji.man.lees@gmail.com>
2014-11-06 14:52:29 +0000

    benjis added

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: "VDbY4GO8HXO4VldZGUcV81qx7",
+    consumer_secret: "nxzFO0R2WSR1MG6x8gL84zwkENUg1HwNHVpPkMW9nmq8ZuuQEZ",
+    access_token: "2690504941-lBFCae0sHvcFH8JiVQ9ZC0J4iUN3FCHU8zCD12D",
+    access_token_secret: "WKaC17gcNLeP85JK82d7sgEdbSbw4s7oAkACEc6tdEKYN"
+};
