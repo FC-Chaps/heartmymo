d67c05fdefb9672221d1c378b0b6f7a74d956a08
benjaminlees <benji.man.lees@gmail.com>
2014-11-07 10:36:51 +0000

    benji

--- twit.js
+++ twit.js
@@ -1,24 +1,27 @@
 var twit = require('twit');
 var feed = new twit(require("./keys.js"));
+var stream = feed.stream('statuses/sample')
 
 module.exports = (function () {
 	return {
-		getTweets:  function (search, validator, number){
-			feed.get("search/tweets", { q: search }, function (err, tweets) {
-				console.log(tweets);
+		getTweets:  function (search, validator, number, callback){
+			feed.get("search/tweets", { q: search}, function (err, tweets) {
+				var tweet = tweets.statuses;
 				var tweetArray = [];
 				var i;
+
 				if (err) {
 					throw err;
 				}
-				for ( i = 0; i < number; i ++){ 
-					tweetArray.push(validator(tweet.statuses[i]));
+				for (i = 0; tweetArray.length < number; i++){ 
+					if(tweet[0] && tweet[i].entities && tweet[i].entities.media && tweet[i].entities.media[0].type === "photo"){
+						tweetArray.push(validator(tweet[i]));
+					};
 				}
-				return tweetArray;
+				callback(tweetArray);	
 			});
 		},
 		validateTweets: function (tweet){
-			if(tweet.entities && tweet.entities.media && tweet.entities.media[0].type === "photo"){
 				var validTweet = {
 					id: tweet.id,
 					username: tweet.user.screen_name,
@@ -27,7 +30,6 @@ module.exports = (function () {
 					body: tweet.text
 				};
 				return validTweet;
-			};
 		}
 	}
 }());
